2063309b8735d896f6abe80f148401bed4b0a488
Iva Havranová <havranova.i@seznam.cz>
2023-05-17 20:28:19 +0200

    Uloha3 done

--- src/Uloha3/index.jsx
+++ src/Uloha3/index.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import dice1Url from './1.svg'
-import dice2Url from './2.svg'
-import dice3Url from './3.svg'
-import dice4Url from './4.svg'
-import dice5Url from './5.svg'
-import dice6Url from './6.svg'
-import './kostka.css'
+import React from 'react';
+import dice1Url from './1.svg';
+import dice2Url from './2.svg';
+import dice3Url from './3.svg';
+import dice4Url from './4.svg';
+import dice5Url from './5.svg';
+import dice6Url from './6.svg';
+import './kostka.css';
+import { useState } from 'react';
 
-const diceUrls = [dice1Url, dice2Url, dice3Url, dice4Url, dice5Url, dice6Url]
+const diceUrls = [dice1Url, dice2Url, dice3Url, dice4Url, dice5Url, dice6Url];
 
 // Zadání 1: Převeď `cisloNaKostce` na stavovou proměnnou.
 // Zadání 2: Po kliku na tlačítko zvětši číslo na kostce o jedna. Po šetce by měla následovat jednička.
@@ -15,19 +16,26 @@ const diceUrls = [dice1Url, dice2Url, dice3Url, dice4Url, dice5Url, dice6Url]
 // Zadání 4: Vypiš v atributu `alt` správné číslo.
 
 export const Uloha3 = () => {
-	const cisloNaKostce = 1 /* jedna až šest */
+  const [diceNumber, setDiceNumber] = useState(1);
 
-	return (
-		<div className="kostka">
-			<img
-				src={diceUrls[cisloNaKostce - 1]} /* indexy se číslují od nuly */
-				width={60}
-				height={60}
-				alt="Kostka s číslem @TODO"
-				className="kostka__ikona"
-			/>
-			<p className="kostka__text">Na kostce je číslo @TODO.</p>
-			<button className="kostka__akce">další</button>
-		</div>
-	)
-}
+  return (
+    <div className="kostka">
+      <img
+        src={diceUrls[diceNumber - 1]} /* indexy se číslují od nuly */
+        width={60}
+        height={60}
+        alt={diceNumber ? `Kostka s číslem ${diceNumber}` : null}
+        className="kostka__ikona"
+      />
+      <p className="kostka__text">Na kostce je číslo {diceNumber}.</p>
+      <button
+        onClick={() => {
+          setDiceNumber(diceNumber <= 5 ? diceNumber + 1 : diceNumber - 5);
+        }}
+        className="kostka__akce"
+      >
+        Zvětši číslo na kostce o jedna.
+      </button>
+    </div>
+  );
+};
